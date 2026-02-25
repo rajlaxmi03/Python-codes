afbeafb0fb4573f9a2e34fd68054e68ea4337d1d
Raj Laxmi <149303703+rajlaxmi03@users.noreply.github.com>
2026-02-25 17:44:51 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,27 @@
+print("Hello,world!")
+print("Hello , python")
+
+# comment -  hello world hello , python
+
+''' multi line comment
+'''
+print("Hello world", end=" - ")
+print("Helo, python")
+
+# my name is Gamana!
+name = "GAmana"
+age = 25
+print(name)
+print("My name is " + name + "!")
+# my name is gamana and i am 25 years olds!
+print("My name is " + name + " and i am " ,age , "years old !") 
+print(3)
+print(3 + 3)
+print("3" + "3")
+
+print("gamana  \n chandu")
+print("abhi")
+
+print("the name is : \t", name)
+print("The age is : " , age)
+
---
+++ variables.py
@@ -0,0 +1,12 @@
+name = "Gamana"
+name = 3
+print(name)
+#a = "Gamana"
+#b = "Gamana"
+#c = "Gamana"
+
+a= b= c = "Gamana"
+print(a, b, c)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
