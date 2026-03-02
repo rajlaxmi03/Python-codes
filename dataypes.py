623f718095fa9854215a19e540e543734c3a1b11
Raj Laxmi <149303703+rajlaxmi03@users.noreply.github.com>
2026-03-02 17:43:45 +0530

    Add files via upload

---
+++ dataypes.py
@@ -0,0 +1,22 @@
+# DATA TYPES
+
+# 1.INTEGER
+age = 21
+print(age)
+print(type(age))
+
+# 2.FLOAT
+marks = 88.65
+print(marks)
+print(type(marks))
+
+# 3.STRING
+name = "gamana"
+print(name)
+print(type(name))
+
+#4.BOOLEAN
+value = True
+print(value)
+print(type(value))
+
