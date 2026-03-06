f02d83545a7fdca3def4bd6288f69fa02e12948e
Raj Laxmi <149303703+rajlaxmi03@users.noreply.github.com>
2026-03-06 17:45:35 +0530

    Add files via upload

---
+++ Trickyoperators.py
@@ -0,0 +1,51 @@
+#More Tricky Programs on python operators
+
+print("1.", 5 + 2 * 3 )   #11
+print("2.", (5 + 2) * 3)   #21
+
+print("3.", 10 / 4)   #2.5
+print("4.", 10 // 4)   #2
+print("5.", 10 % 4)     #2
+
+print("6.", 2 ** 3 ** 2)
+
+print("7.", True + 5)
+print("8.", False * 10)
+
+print("9.", 10 and 0)
+print("10.", 10 and 5)
+
+print("11.", not 0)
+print("12.", 0 or 5 or 10)
+
+print("13.", not 0)
+print("14.", not 10)
+
+print("15.", 5 > 3 > 1)
+print("16.", 5 > 3 < 4)
+
+print("17.", 5 == 5.0)
+print("18.", 5 is 5.0)
+
+print("19.", -5 % 3)
+print("20.", 5 % -3)
+
+x = 10
+x += 5
+print("21.", x)
+
+x += 2
+print("22.", x)
+
+print("23.", 4 + 3 * 2 ** 2)   #16
+print("24.", (4 + 3) * 2 ** 2)  #28
+
+print("25.", True == 1)    #True
+print("26.", False == 0)   #True
+
+print("27.", 3 < 5 == True)  #False
+
+print("28.", bool(0))    #False
+print("29.", bool(-1))    #True
+
+print("30.", not True == False)  #true
